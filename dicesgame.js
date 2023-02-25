cb4a51cb269229ef4e5a4d068ba5348b8b346d1a
HassanZeinAlDeen <hassanzeinaldeen0@gmail.com>
2023-02-25 18:58:45 +0200

    adding function determine winner

--- dicesgame.js
+++ dicesgame.js
@@ -1,4 +1,22 @@
-var x = Math.floor(Math.random()*(6 - 1 + 1)) + 1;
-var y = Math.floor(Math.random()*(6 - 1 + 1)) + 1;
-console.log(x , y)
+window.onload = function () {
+  document.addEventListener("load", Function)
+  var x = Math.floor(Math.random() * (6 - 1 + 1)) + 1;
+  var y = Math.floor(Math.random() * (6 - 1 + 1)) + 1;
+  document.getElementById("n_1").innerHTML = x;
+  document.getElementById("n_2").innerHTML = y;
+  var winner = document.querySelector(".game_title");
+  dtrmnWinner(x, y);
 
+
+  function dtrmnWinner(x, y) {
+    if (x > y) {
+      winner.textContent = "Player 1 Wins"
+    }
+    else if (x == y) {
+      winner.textContent = "Draw"
+    }
+    else {
+      winner.textContent = "Player 2 Wins"
+    }
+  }
+}
